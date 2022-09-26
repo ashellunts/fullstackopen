37da9a4db579c28f8d7b3c57fcc4d09e32dfedf5
Artur Shellunts <shellunts.artur@gmail.com>
2022-09-26 09:16:58 +0200

    2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const App = () => {
 
-  const [searchString, setSearchString] = useState('')
+  const [searchString, setSearchString] = useState('germa')
   const [countries, setCountries] = useState([])
 
   useEffect(() => {
@@ -13,20 +13,39 @@ const App = () => {
         setCountries(response.data)
       })
   }, [])
-  
+
   const countriesFound = countries
                             .filter(country => country.name.common.toUpperCase().includes(searchString.toUpperCase()))
-                            .map(country => <li key={country.name.common}>{country.name.common}</li>)
-  
-  const countriesToShow = countriesFound.length > 10
+
+  let footer = ""
+  if (countriesFound.length === 1) {
+    const country = countriesFound[0]
+    footer = (
+      <>
+        <h2> {country.name.common} </h2>
+        <b>languages:</b>
+        <ul>
+          {Object
+            .keys(country.languages)
+            .map(languageKey => <li key={country.name.common} > {country.languages[languageKey]} </li>)
+          }
+        </ul>
+        <img src={country.flags.png} alt={country.name.common} />
+      </>
+    )
+  } else {
+    footer = countriesFound.length > 10
                             ? <p>Too many matches</p>
-                            : <ul> {countriesFound} </ul>
+                            : <ul>
+                                {countriesFound.map(country => <li key={country.name.common}>{country.name.common}</li>)}
+                              </ul>
+  }
 
   return (
     <div>
       <p>find countries</p>
       <input value={searchString} onChange={event => setSearchString(event.target.value)} />
-      {countriesToShow}
+      {footer}
     </div>
   )
 }
