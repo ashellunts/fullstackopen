aee3dd14f86e4cdcec14f6be0710a69fe63c126a
Artur Shellunts <shellunts.artur@gmail.com>
2022-09-27 19:56:58 +0200

    next

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,13 +1,34 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const Weather = ({weatherReport}) => {
+const WeatherInCaptial = ({country}) => {
+  const [temperature, setTemperature] = useState('')
+  const [wind, setWind] = useState('')
+  const [icon, setIcon] = useState('')
+  const [description, setDescription] = useState('')
+
+  const lat = country.capitalInfo.latlng[0]
+  const lng = country.capitalInfo.latlng[1]
+
+  const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=0da4deda71d535e40e7543287af92b79`
+
+  axios
+      .get(api_url)
+      .then(response => {
+        const weatherReport = response.data
+        setTemperature(weatherReport.main.temp)
+        setWind(weatherReport.wind.speed)
+        setIcon(`https://openweathermap.org/img/wn/${weatherReport.weather[0].icon}@2x.png`)
+        setDescription(weatherReport.weather[0].description)
+      })
+
   return (
     <div>
-      <h3> Weather in Xxx </h3>
-      <p> Temperate xxx </p>
-      <img src="link" alt="weather img" />
-      <p> Wind xxx </p>
+      <h3> Weather in {country.capital[0]} </h3>
+      <p> temperate {temperature} C. </p>
+      <p> wind {wind} m/s </p>
+      <img src={icon} alt={description} />
+      <p> {description} </p>
     </div>
   )
 }
@@ -19,17 +40,16 @@ const Country = ({country}) => {
   return (
     <div>
       <h2> {country.name.common} </h2>
-      <p> Capital xxx </p>
-      <p> Area xxx </p>
+      <p> capital {country.capital[0]} </p>
       <b>languages:</b>
       <ul>
         {Object
           .keys(country.languages)
-          .map(languageKey => <li key={country.name.common} > {country.languages[languageKey]} </li>)
+          .map(languageKey => <li key={languageKey} > {country.languages[languageKey]} </li>)
         }
       </ul>
       <img src={country.flags.png} alt={country.name.common} />
-      <Weather />
+      <WeatherInCaptial country={country} />
     </div>
   )
 }
