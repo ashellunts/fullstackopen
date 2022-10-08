1c3b666d4fec481daf88c83ba3ecc3828544a843
Artur Shellunts <artur.shellunts@profiforms.de>
2022-10-08 11:52:03 +0200

    format code

--- test/index.js
+++ test/index.js
@@ -25,29 +25,29 @@ let notes = [
 
 const generateId = () => {
     const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
     return maxId + 1
 }
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
-  
+
     if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+        return response.status(400).json({
+            error: 'content missing'
+        })
     }
-  
+
     const note = {
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
-      id: generateId(),
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
     }
-  
+
     notes = notes.concat(note)
-  
+
     response.json(note)
 })
 
